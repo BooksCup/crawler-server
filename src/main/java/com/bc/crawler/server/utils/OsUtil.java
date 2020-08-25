47502abd049d73fb63681259139904848daa0255
BooksCup <812809680@qq.com>
2020-08-25 15:26:19 +0800

    更新获取ip地址工具类代码

--- src/main/java/com/bc/crawler/server/utils/OsUtil.java
+++ src/main/java/com/bc/crawler/server/utils/OsUtil.java
@@ -1,7 +1,14 @@
 package com.bc.crawler.server.utils;
 
-import java.net.InetAddress;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 操作系统工具类
@@ -25,13 +32,37 @@ public class OsUtil {
      * @return IP地址
      */
     public static String getIp() {
-        String ip;
+        String ip = "";
+        String chinaz = "http://ip.chinaz.com";
+        StringBuilder inputLine = new StringBuilder();
+        String read;
+        URL url;
+        HttpURLConnection urlConnection = null;
+        BufferedReader in = null;
         try {
-            InetAddress inetAddress = InetAddress.getLocalHost();
-            ip = inetAddress.getHostAddress();
-        } catch (Exception e) {
+            url = new URL(chinaz);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+            while ((read = in.readLine()) != null) {
+                inputLine.append(read + "\r\n");
+            }
+        } catch (MalformedURLException e) {
             e.printStackTrace();
-            ip = "";
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        Pattern p = Pattern.compile("\\<dd class\\=\"fz24\">(.*?)\\<\\/dd>");
+        Matcher m = p.matcher(inputLine.toString());
+        if (m.find()) {
+            ip = m.group(1);
         }
         return ip;
     }
